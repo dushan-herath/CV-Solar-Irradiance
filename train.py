f4617c32b3de9acd52556940957c3f4eeff2c533
dushan-herath <dushan.herath123@icloud.com>
2025-11-12 20:48:56 -0600

    update

--- train.py
+++ train.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     mp.freeze_support()  
 
     # --- Config ---
-    CSV_PATH = "processed_dataset_cropped_sanity.csv"
-    BATCH_SIZE = 8
+    CSV_PATH = "processed_dataset_polar_full.csv"
+    BATCH_SIZE = 128
     NUM_EPOCHS = 25
     LR = 1e-4
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -127,8 +127,8 @@ if __name__ == "__main__":
     )
 
 
-    train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, pin_memory=True)
-    val_loader = DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=1, pin_memory=True)
+    train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
+    val_loader = DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)
 
     # --- Model setup ---
     img_encoder = ImageEncoder(model_name="vit_small_patch16_224", pretrained=True, freeze=True)
