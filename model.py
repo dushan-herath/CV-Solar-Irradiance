112c971d4dde2c421025499622c2ab7d31db2765
dushan-herath <dushan.herath123@icloud.com>
2025-11-14 21:46:51 -0600

    update

--- model.py
+++ model.py
@@ -178,6 +178,9 @@ class MultimodalForecaster(nn.Module):
         img_feats = img_feats_flat.view(B, T_img, -1)
         img_feats = self.img_pos_enc(img_feats)
 
+        # temp disable image encoder
+        img_feats = torch.zeros(B, T_img, self.img_embed_dim, device=ts.device)
+
         # Encode TS
         ts_feats = self.ts_encoder(ts)
         ts_feats = self.ts_pos_enc(ts_feats)
